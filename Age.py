b7d803a387f6bd987261f499084b2674bf348452
Gowthamkasturi <gowthamkasturi464@gmail.com>
2024-12-26 12:25:14 +0530

    Add files via upload

---
+++ Age.py
@@ -0,0 +1,6 @@
+Age=int(input("Enter the Age:"))
+#Condition to check the eligibility to vote
+if(Age>=18):
+    print("Eligible to vote....")
+else:
+    print(" your not Eligible to votting......")
